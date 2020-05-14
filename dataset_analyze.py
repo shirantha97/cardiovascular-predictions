5fd853d751f7e03de50e26a0ca29eb10aaad556b
shirantha97 <herathshirantha@gmail.com>
2020-05-14 14:56:18 +0530

    create test,train data

--- dataset_analyze.py
+++ dataset_analyze.py
@@ -3,7 +3,7 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import joblib as jb
-from pandas._libs.missing import NA
+# from pandas._libs.missing import NA
 from sklearn.linear_model import LogisticRegression
 import sys
 
@@ -22,8 +22,18 @@ sns.heatmap(
 print(sum(cardioVascularDF.isnull().sum()))
 # replace Nan values with the mean column value
 cardioVascularDF.fillna(cardioVascularDF.mean, inplace=True)
-print(sum(cardioVascularDF.isnull().sum()))
+print('the sum null values ' + str(sum(cardioVascularDF.isnull().sum())))
 print(sys.getrecursionlimit())
 sys.setrecursionlimit(1500)
 # check if Nan values are replaced in a random column
 print((cardioVascularDF['education'].isnull().sum()))
+
+dfTrain = cardioVascularDF[:3000]
+dfTest = cardioVascularDF[3000:4000]
+dfCheck = cardioVascularDF[4000:]
+
+trainLbl = np.asarray(dfTrain['TenYearCHD'])
+trainData = np.asarray(dfTrain.drop('TenYearCHD', 1))
+# print(trainData)
+testLbl = np.asarray(dfTest['TenYearCHD'])
+testData = np.asarray(dfTest.drop('TenYearCHD', 1))
