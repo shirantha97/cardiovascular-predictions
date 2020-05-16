a02e7fb15d4a45ad6d7791966521155585e665a3
shirantha97 <herathshirantha@gmail.com>
2020-05-16 17:24:08 +0530

    feature selection

--- dataset_analyze.py
+++ dataset_analyze.py
@@ -1,16 +1,16 @@
 import pandas as pd
 import numpy as np
 import seaborn as sns
-import matplotlib.pyplot as plt
-import joblib as jb
-# from pandas._libs.missing import NA
+from statistics import mode
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
 from sklearn.linear_model import LogisticRegression
-import sys
+from sklearn.model_selection import train_test_split
 
-cardioVascularDF = pd.read_csv('Dataset1 - cardio-vascular dataset.csv')
-# print(cardioVascularDF.head(20))
+cardioVascularDF = pd.read_csv('Dataset1 - cardio-vascular dataset.csv', index_col=False)
 cardioVascularDF.info()
 cardioVascularDF.describe().transpose()
+
 corr = cardioVascularDF.corr()
 print(corr)
 sns.heatmap(
@@ -18,22 +18,37 @@ sns.heatmap(
     xticklabels=corr.columns,
     yticklabels=corr.columns
 )
-# check for null values
-print(sum(cardioVascularDF.isnull().sum()))
-# replace Nan values with the mean column value
-cardioVascularDF.fillna(cardioVascularDF.mean, inplace=True)
-print('the sum null values ' + str(sum(cardioVascularDF.isnull().sum())))
-print(sys.getrecursionlimit())
-sys.setrecursionlimit(1500)
-# check if Nan values are replaced in a random column
-print((cardioVascularDF['education'].isnull().sum()))
 
-dfTrain = cardioVascularDF[:3000]
-dfTest = cardioVascularDF[3000:4000]
-dfCheck = cardioVascularDF[4000:]
+print(cardioVascularDF.describe(include="all"))
+cardioVascularDF["education"] = cardioVascularDF["education"].fillna(mode(cardioVascularDF["education"]))
+cardioVascularDF["cigsPerDay"] = cardioVascularDF["cigsPerDay"].fillna(cardioVascularDF["cigsPerDay"].mean())
+cardioVascularDF["BPMeds"] = cardioVascularDF["BPMeds"].fillna(mode(cardioVascularDF["BPMeds"]))
+cardioVascularDF["totChol"] = cardioVascularDF["totChol"].fillna(cardioVascularDF["totChol"].mean())
+cardioVascularDF["glucose"] = cardioVascularDF["glucose"].fillna(cardioVascularDF["glucose"].mean())
+cardioVascularDF["BMI"] = cardioVascularDF["BMI"].fillna(cardioVascularDF["BMI"].mean())
+cardioVascularDF["heartRate"] = cardioVascularDF["heartRate"].fillna(cardioVascularDF["heartRate"].mean())
 
-trainLbl = np.asarray(dfTrain['TenYearCHD'])
-trainData = np.asarray(dfTrain.drop('TenYearCHD', 1))
-# print(trainData)
-testLbl = np.asarray(dfTest['TenYearCHD'])
-testData = np.asarray(dfTest.drop('TenYearCHD', 1))
+# check if there are still Nan values in the detected columns
+arr = np.asarray(cardioVascularDF['glucose'])
+print(arr[14])
+print("sum of nan values " + str(cardioVascularDF.isna().sum()))
+a = False
+for col in cardioVascularDF.columns:
+    if cardioVascularDF[col].isnull().any():
+        a = True
+if a:
+    print('null values in the dataset')
+
+print('nan values   ' + str(cardioVascularDF.isna().sum()))
+
+data = pd.DataFrame(cardioVascularDF.values)
+X_features = cardioVascularDF.drop(["TenYearCHD"], axis=1)
+Y_features = cardioVascularDF["TenYearCHD"]
+
+bestFeatures = SelectKBest(score_func=chi2, k=10)
+fit = bestFeatures.fit(X_features, Y_features)
+dataScore = pd.DataFrame(fit.scores_)
+dataColumns = pd.DataFrame(X_features.columns)
+featureScore = pd.concat([dataColumns, dataScore], axis=1)
+featureScore.columns = ["feature", "score"]
+print(featureScore)
